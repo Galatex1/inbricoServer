6b80afe2f925938ade97dae91a10e4b9bccc83f4
galatex1 <f.galatex@gmail.com>
2018-11-25 16:48:08 +0100

    Hotfix for queue, first item in  queue crash, cause there was no finish time for previous one.

--- routes/buildings.js
+++ routes/buildings.js
@@ -222,15 +222,23 @@ module.exports = function(app){
                             getBuilding(result[0]['build'], uplevel).then((res) =>{               
                         
                                 updatePlayerResources(playerID, res[0]['wood'], res[0]['stone'], res[0]['iron'], res[0]['gold']).then((val)=>{
-                                        getLastItem(playerID).then((complete)=>{
-
+                                        getLastItem(playerID).then((last)=>{
+                                                                               
                                             if(val > 0)
                                             {
-                                            
-                                                //let sql = "INSERT INTO building_queue (player_id, tile_id, build, level, complete) VALUE(?, ?, ?, ?, DATE_ADD(NOW(), INTERVAL TIME_TO_SEC(?) SECOND))";
-                                                let sql = "INSERT INTO building_queue (player_id, tile_id, build, level, complete, start) VALUE(?, ?, ?, ?, DATE_ADD(?, INTERVAL TIME_TO_SEC(?) SECOND), ?)";
-                
-                                                let inserts = [playerID, hexID, result[0]['build'], res[0]['level'],complete[0]['complete'], res[0]['time'],complete[0]['complete']];
+                                                let sql;
+                                                let inserts;
+                                                
+                                                if(last.length == 0)
+                                                {
+                                                    sql = "INSERT INTO building_queue (player_id, tile_id, build, level, complete, start) VALUE(?, ?, ?, ?, DATE_ADD(NOW(), INTERVAL TIME_TO_SEC(?) SECOND), NOW())";
+                                                    inserts = [playerID, hexID, result[0]['build'], res[0]['level'], res[0]['time']]; 
+                                                }
+                                                else
+                                                {
+                                                    sql = "INSERT INTO building_queue (player_id, tile_id, build, level, complete, start) VALUE(?, ?, ?, ?, DATE_ADD(?, INTERVAL TIME_TO_SEC(?) SECOND), ?)";                   
+                                                    inserts = [playerID, hexID, result[0]['build'], res[0]['level'], last[0]['complete'], res[0]['time'], last[0]['complete']];
+                                                }
                                         
                                                 sql = mysql.format(sql, inserts);
                                         
@@ -275,7 +283,7 @@ module.exports = function(app){
         
         return new Promise(function(resolve, reject) {
 
-            let sql = "SELECT name, player_id, tile_id, build, level, start, complete, queued, TIME_TO_SEC(TIMEDIFF(complete, start)) as time, TIME_TO_SEC(TIMEDIFF(complete, NOW())) as remain FROM building_queue LEFT JOIN player ON player_id = player.ID LEFT JOIN buildings ON buildings.id = build WHERE ?? = ?";
+            let sql = "SELECT name, player_id, tile_id, build, level, start, complete, queued, TIME_TO_SEC(TIMEDIFF(complete, start)) as time, TIME_TO_SEC(TIMEDIFF(complete, NOW())) as remain FROM building_queue LEFT JOIN player ON player_id = player.ID LEFT JOIN buildings ON buildings.id = build WHERE ?? = ? ORDER BY complete ASC";
 
             let inserts = transformPlayer(player);
     
