cc5484637c87b9230db1ed724d2ee6d65903100d
galatex1 <f.galatex@gmail.com>
2019-03-19 22:15:19 +0100

    ...

--- server.js
+++ server.js
@@ -45,9 +45,10 @@ app.listen(process.env.PORT || 8000, () => {
   console.log('Server started!');
 });
 
+app.use(express.static(__dirname + '/public/'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile('index.html');
 });
 
 app.post('/login',function(request,response, next){
